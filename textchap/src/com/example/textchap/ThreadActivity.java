4b069f81b5b5f602bd42f2c04e3bdff4b0811edf
Jasdeep Garcha <jasdeepsgarcha@gmail.com>
2013-06-22 13:19:57 -0700

    ThreadActivity

--- textchap/src/com/example/textchap/ThreadActivity.java
+++ textchap/src/com/example/textchap/ThreadActivity.java
@@ -9,7 +9,7 @@ public class ThreadActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	
+	    
 	    // TODO Auto-generated method stub
 	}
 
